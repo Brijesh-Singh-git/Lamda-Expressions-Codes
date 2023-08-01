ef43c6532aaf19960e184ec4f2e4d4f6cbf6719e
Brijesh Singh <106757278+Brijesh-Singh-git@users.noreply.github.com>
2023-08-01 07:52:33 +0530

    com

---
+++ src/Predicates/Demo3.java
@@ -0,0 +1,31 @@
+package Predicates;
+
+import java.util.function.Predicate;
+
+//Joining predicates - and , or, nor
+//p1  -> checks number is even
+//p2  -> checks greater than 50 
+
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		
+		int a[]= {5,15,2,45,78,67,90,32,43,78,55,65,89,104,22,92,13};
+		
+		Predicate<Integer> p1 =i->i%2==0;
+		Predicate<Integer> p2 =i->i>50;
+		
+		//and
+		System.out.println("Following are numbers EVEN & Greater than 50 .....");
+		
+		for(int n:a) {
+			
+			if(p1.and(p2).test(n)) { //if(p1.test(n) && p2.test(n))
+				
+				System.out.println(n);
+			}
+		}
+	}
+
+}
